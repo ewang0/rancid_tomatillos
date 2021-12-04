fa77080d51d2a3ff4b9e4bee559b878be7042d8c
Eric Wang <erwang1151@gmail.com>
2021-12-04 13:16:33 -0500

    Basic styling of nav bar and movie section

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import './MovieCard.scss';
 
-const MovieCard = ({id, title, averageRating}) => {
-  return (
-    <div className='card'>
-      <h3>{title}</h3>
-      <p>{averageRating}</p>
-    </div>
-  )
+const MovieCard = ({id, poster_path, title, average_rating, release_date}) => {
+    const year = release_date.slice(0,4);
+    return (
+        <div className='card'>
+            <img src={poster_path}/>
+            <h3>{title}</h3>
+            <p>{year}</p>
+            <p>{average_rating}</p>
+
+        </div>
+    )
 }
 
 export default MovieCard;
--- src/MovieSection.js
+++ src/MovieSection.js
@@ -9,13 +9,15 @@ const MovieSection = (props) => {
       <MovieCard
         id={movie.id}
         title={movie.title}
-        averageRating={movie.average_rating}
+        average_rating={movie.average_rating}
+        poster_path={movie.poster_path}
+        release_date={movie.release_date}
       />
     )
   })
 
   return (
-    <div className='movies-container'>
+    <div className='movie-container'>
       {createCards}
     </div>
   )
