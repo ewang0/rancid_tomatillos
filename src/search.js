d4ae81fb428399ccf003d200887c686ecba80260
Edward Krupicka <edwardkrupicka@gmail.com>
2021-12-07 19:20:06 -0700

    Add styling for search bar

--- src/search.js
+++ src/search.js
@@ -2,16 +2,18 @@ import './search.scss'
 
 const Search = (props) => {
 	return (
-	<form action="/" method="get">
+	<form className="searchContainer" action="/" method="get">
         <label htmlFor="header-search">
             <span className="visually-hidden">Search Movies</span>
         </label>
+        <img className="icon" src="https://www.kindacode.com/wp-content/uploads/2020/12/search.png"/>
         <input
+            className="searchBar"
             type="text"
             id="header-search"
-            placeholder="Search movies"
+            placeholder="Search..."
 			onChange={props.handleChange}
-            name="s" 
+            name="s"
         />
     </form>
 	)
