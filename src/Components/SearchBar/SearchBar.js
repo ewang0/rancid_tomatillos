ddf2de47b1b2c5dca791e6bb7cb1d840364fcd79
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2021-12-10 13:13:59 -0700

    Rename searchbar and changed visiblity

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './SearchBar.scss'
+import { NavLink, useLocation } from 'react-router-dom';
+
+const SearchBar = (props) => {
+    const currentPath = useLocation().pathname === '/' ? '/search' : '/'
+    console.log(currentPath)
+
+	return (
+	<form className="searchContainer" action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search Movies</span>
+        </label>
+        <NavLink to={ currentPath }><img className="icon" src="https://www.svgrepo.com/show/146074/magnifying-glass.svg"/></NavLink>
+        <input
+            className={ currentPath === '/' ? 'searchBar show' : 'searchBar' }
+            type="text"
+            id="header-search"
+            placeholder="Search..."
+			onChange={props.handleChange}
+            autoComplete="off"
+        />
+    </form>
+	)
+}
+
+export default SearchBar;
