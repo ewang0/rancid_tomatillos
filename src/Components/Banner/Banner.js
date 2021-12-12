3ff5baa2c24ff83b492352103d8d1c2a275013b6
Eric Wang <erwang1151@gmail.com>
2021-12-12 14:21:54 -0500

    Style banner and enable autoplay

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Banner.scss';
 import ReactStars from 'react-stars';
 import './react-carousel.scss';
-import { getSingleMovie } from '../../apiCalls';
 import { Carousel } from 'react-responsive-carousel';
 
 const Banner = ({ data }) => {
-    const randomBanners = data.map((movieObj) => {
+    const shuffledData = data.sort((a,b) => 0.5-Math.random());
+    const randomBanners = shuffledData.map((movieObj) => {
         return (
                 <section className="banner">
                     <div className="banner-info">
@@ -15,7 +15,7 @@ const Banner = ({ data }) => {
                             <ReactStars count={5} value={movieObj.average_rating / 2} size={15} color2={'lightgrey'} />
                         </div>
                         <p>{movieObj.overview}</p>
-                        <p>{movieObj.runtime}</p>
+                        <p><b>{movieObj.runtime} Minutes</b></p>
                     </div>
                     <div className="banner-image-wrapper">
                         <img src={movieObj.backdrop_path}/>
@@ -26,7 +26,7 @@ const Banner = ({ data }) => {
     })
 
     return(
-        <Carousel showArrows={true}>
+        <Carousel showArrows={true} autoPlay={true} interval={3000} infiniteLoop={true} showIndicators={false} showStatus={false} showThumbs={false}>
             {randomBanners}
         </Carousel>
         
