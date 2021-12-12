9b6c3fc2f4c33cd02815232afe15678870930c38
Edward Krupicka <87044013+edwardkrupicka@users.noreply.github.com>
2021-12-12 12:36:40 -0700

    refactor

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -37,7 +37,7 @@ const Banner = ({ data }) => {
     })
 
 const retrieveBanners = () => {
-    if(randomBanners.length === 40){
+    if(randomBanners.length === 37){
         return randomBanners
     }
 }
